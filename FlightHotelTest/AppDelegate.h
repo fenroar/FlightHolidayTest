e8f3794c139a07130eaa06edadc6208e7d8ebff2
Peter Su <fenroar@gmail.com>
2016-11-23 10:45:35 +0000

    Initial commit

---
+++ FlightHotelTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlightHotelTest
+//
+//  Created by Peter Su on 23/11/2016.
+//  Copyright © 2016 fenroar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlightHotelTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlightHotelTest
+//
+//  Created by Peter Su on 23/11/2016.
+//  Copyright © 2016 fenroar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
